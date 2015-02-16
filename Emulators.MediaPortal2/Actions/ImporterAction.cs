c30c9bfe6aad28d313ad079be37239808e9d6dfc
Brownard <brownard@live.co.uk>
2015-02-16 14:29:01 +0000

    Add view importer action

---
+++ Emulators.MediaPortal2/Actions/ImporterAction.cs
@@ -0,0 +1,51 @@
+﻿using Emulators.MediaPortal2.Models.Dialogs;
+using Emulators.MediaPortal2.Settings;
+using MediaPortal.Common;
+using MediaPortal.Common.Commands;
+using MediaPortal.Common.Localization;
+using MediaPortal.UI.Presentation.DataObjects;
+using MediaPortal.UI.Presentation.Workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulators.MediaPortal2.Actions
+{
+    class ImporterAction : IWorkflowContributor
+    {
+        public IResourceString DisplayTitle
+        {
+            get { return LocalizationHelper.CreateResourceString("[Emulators.Importer]"); }
+        }
+
+        public void Execute()
+        {
+            IWorkflowManager workflowManager = ServiceRegistration.Get<IWorkflowManager>();
+            workflowManager.NavigatePushAsync(Guids.ImporterState);
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public bool IsActionEnabled(NavigationContext context)
+        {
+            return true;
+        }
+
+        public bool IsActionVisible(NavigationContext context)
+        {
+            return context.WorkflowModelId == Guids.WorkflowSatesMain;
+        }
+
+        public event ContributorStateChangeDelegate StateChanged;
+
+        public void Uninitialize()
+        {
+
+        }
+    }
+}
--- Emulators.MediaPortal2/Guids.cs
+++ Emulators.MediaPortal2/Guids.cs
@@ -10,7 +10,9 @@ namespace Emulators.MediaPortal2
     {
         public static readonly Guid WorkflowSatesMain = new Guid("3C370162-2586-416A-963C-F34DDDE184D4");
         public static readonly Guid WorkflowSatesEmulators = new Guid("97F9A2D9-B857-4890-9EF2-5E2BC66A53BB");
+
         public static readonly Guid ImporterModel = new Guid("C27FA8F9-500C-4AA3-83CB-93427ED1B2EA");
+        public static readonly Guid ImporterState = new Guid("1782275A-4B3C-4E00-9F97-8B60DFD28F91");
 
         public static readonly Guid NewEmulatorWorkflow = new Guid("03F534A5-4495-4223-A7EB-0FC8D477CF4B");
         public static readonly Guid NewEmulatorState = new Guid("C69A5500-0059-4E4E-A331-2FD9835C4D0B");
